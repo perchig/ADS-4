b72e66bded3d8211a8c12f5ed19683991543a52d
perchig <ivanslepko11@gmail.com>
2025-04-14 23:19:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -10,25 +10,25 @@ int countPairs1(int *arr, int len, int value) {
 
   return count;
 }
-int countPairs2(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len; i++) {
-    if (arr[i] >= value)
-      continue;
-    for (int j = len - 1; j > i; j--) {
-      if (arr[i] + arr[j] > value)
-        continue;
-      else if (arr[i] + arr[j] < value)
-        break;
-      count++;
-      int k = j - 1;
-      while (k > i && arr[k] == arr[j]) {
-        count++;
-        k--;
-      }
-      break;
+nt countPairs2(int *array, int len, int target) {
+    int count = 0;
+    int upperBound = len - 1;
+
+    while (upperBound > 0 && array[upperBound] > target) {
+        upperBound--;
     }
-  }
+
+    for (int l = 0; l < len; l++) {
+        for (int r = upperBound; r > l; r--) {
+            if (array[l] + array[r] == target) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
 
   return count;
 }
