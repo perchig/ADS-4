b3d4f723c14f06be8ce8196f22d67ea94e9d4b73
perchig <ivanslepko11@gmail.com>
2025-04-14 22:11:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,8 @@
+// Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len ; i++){
-    for (int j = i + 1; j < len; j++){
+  for (int i = 0; i < len ; i++) {
+    for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value)
         count++;
     }
