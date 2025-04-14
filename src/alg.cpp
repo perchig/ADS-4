415a51c40d2e409b273227f9198719febed38e84
perchig <ivanslepko11@gmail.com>
2025-04-14 21:19:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,55 @@
-// Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len ; i++){
+    for (int j = 0; j < len; j++){
+      if (arr[i] + arr[j] == value)
+        count++;
+    }
+  }
+
+  return count;
 }
+
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len ; i++){
+    if (arr[i] >= value)
+      continue;
+    for (int j = len - 1; j >= i; j--){
+      if (arr[i] + arr[j] > value)
+        continue;
+      else if (arr[i] + arr[j] < value)
+        break;
+      else {
+        count++;
+        break;
+      }
+    }
+  }
+
+  return count;
 }
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len; i++){
+    int target = value - arr[i];
+    if (arr[i] > value && value > 0)
+      continue;
+    int left = i + 1;
+    int right = len - 1;
+    while (left <= right){
+      int mid = (left + right) / 2;
+      if (arr[mid] == target){
+        count++;
+        break;
+      }
+      else if (arr[mid] < target)
+        left = mid + 1;
+      else
+        right = mid - 1;
+    }
+  }
+  
+  return count;
 }
