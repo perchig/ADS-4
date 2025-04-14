180f4fa7c17a5cf41fb68dd4d26196198ae7d5bb
perchig <ivanslepko11@gmail.com>
2025-04-14 23:03:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,7 +12,6 @@ int countPairs1(int *arr, int len, int value) {
 }
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
-  int *tempArr = new int[len];
   for (int i = 0; i < len; i++) {
     if (arr[i] >= value)
       continue;
